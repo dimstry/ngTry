0e73e5922d08d88f63c346d52942c62dc7e7af4e
dimstry <dimastriana03@gmail.com>
2022-08-12 11:34:05 +0700

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Zilong' },
+  { id: 13, name: 'Balmon' },
+  { id: 14, name: 'Argus' },
+  { id: 15, name: 'Selena' },
+  { id: 16, name: 'Nana' },
+  { id: 17, name: 'Kufra' },
+  { id: 18, name: 'Asta' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
